e573a3875e1c4a4946fa83daef7f21a35cdb2a4e
Matt <mw283@cam.ac.uk>
2017-02-08 21:01:08 +0000

    new file:   dockerized-tools/ballgown/Dockerfile 	new file:   dockerized-tools/ballgown/ballgown-test.R 	new file:   dockerized-tools/ballgown/setup.r

---
+++ dockerized-tools/ballgown/setup.r
@@ -0,0 +1,13 @@
+#!/usr/bin/env r 
+
+# load utils package
+library("utils")
+
+# install packages from CRAN
+install.packages(c("dplyr", "RColorBrewer", "RCurl"))
+
+# install packages from bioconductor
+source("https://bioconductor.org/biocLite.R")
+biocLite(ask=FALSE)
+biocLite(c("ballgown","genefilter"), ask=FALSE)
+update.packages(ask=FALSE)
